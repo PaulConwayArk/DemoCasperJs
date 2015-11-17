0b7428e6f3df854175159addbcb785d9fefb039c
PaulConwayArk <paul.conway@ark-energy.eu>
2015-11-17 15:43:23 +0000

    Test

---
+++ CasperJsDemo/CasperJsDemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasperJsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Echo(string shout)
+        {
+            ViewBag.Shout = shout;
+            return View("Echo");
+        }
+    }
+}
---
+++ CasperJsDemo/CasperJsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CasperJsDemo.MvcApplication" Language="C#" %>
